1e860b9a89c34435e42fb8c0275fe9254301e1fb
Jeremy <jeremygrondahl@gmail.com>
2015-08-24 13:10:32 -0500

    Changes to files.

--- music-history-5/javascripts/get-more-songs.js
+++ music-history-5/javascripts/get-more-songs.js
@@ -1,11 +1,18 @@
-define(["jquery"],function($) {
-  return {
-    querySongs: function (callback){
-      $.ajax({
-        url:"/data/list2.json",
-      }).done(function (data) {
-        callback.call(this, data);
-      });
-    }
+define(["jquery", "q"], function($, Q) {
+  var deferred = Q.defer();
+  return function () {
+
+    $.ajax({
+      url: "/data/list2.json"
+    })
+    .done(function(songs_data) {
+      deferred.resolve(songs_data);
+    })
+    .fail(function(xhr, status, error){
+      deferred.reject(error);
+    });
+
+    return deferred.promise;
   };
+    
 });
--- music-history-5/javascripts/main.js
+++ music-history-5/javascripts/main.js
@@ -4,45 +4,44 @@ requirejs.config({
   paths: {
     'jquery': '../bower_components/jquery/dist/jquery.min',
     'hbs': '../bower_components/require-handlebars-plugin/hbs',
-    'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min'
+    'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min',
+    'firebase': '../bower_components/firebase/firebase',
+    'lodash': '../bower_components/lodash/lodash.min',
+    'q': '../bower_components/q/q'
   },
   shim: {
-    'bootstrap': ['jquery']
-  }
-});
-/* requirejs is passing the named JS files as arguments 
-calling back their methods from inside another function into the main.js file. */ 
-requirejs(["jquery", "hbs", "bootstrap", "dom-access", "populate-songs", "get-more-songs", "addSong"], 
-  function($, Handlebars, bootstrap, dom, populate, get, addSong) {
-  
-// Callingback information from it's file. 
-    populate.querySongs(function (songs){
-      music(songs);
-      fullList(songs);
-    });
-  $(".matchheight").matchHeight();
-    $("#main").on("click",".delButton", function() {
-      $(this).closest("div").remove();
-    });
-  function fullList(songs){
-    get.querySongs(function (songs){
-      require(['hbs!../templates/artist', 'hbs!../templates/album'], 
-      function(artistTemplate, albumTemplate) {
-        $("#artist").append(artistTemplate(songs));
-        $("#album").append(albumTemplate(songs));
-      });
-    });
+    'bootstrap': ['jquery'],
+    'firebase': {
+      exports: 'Firebase'
+    }
   }
 });
 
-function music(songs){
-  require(['hbs!../templates/songs', 'hbs!../templates/artist', 'hbs!../templates/album' ], 
-    function(songTemplate, artistTemplate, albumTemplate) {
-      $("#artist").append(artistTemplate(songs));
-      $("#album").append(albumTemplate(songs));
-      $("#main").append(songTemplate(songs));
-  });
-}
+requirejs(["jquery", "lodash", "hbs", "bootstrap", "dom-access",  "addSong", "filterSong", "firebase", "q", "get-songs", "get-more-songs", "searchArtist", "authentication"], 
+  function($, _, Handlebars, bootstrap, dom, addSong, filterSong, _firebase, q, getSongs, getMoreSongs, search, auth) {
+
+  var ref = new Firebase("https://blazing-heat-6599.firebaseio.com");
+  var authData = ref.getAuth();
+  console.log("authData", authData);
+
+  if(authData === null) {
+    ref.authWithOAuthPopup("github", function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+        auth.setUid(authData.uid);
+        require(["core_list"], function() {});
+      }
+    });
+
+  } else {
+        console.log("Authenticated successfully with payload:", authData);
+        auth.setUid(authData.uid);
+        require(["core_list"], function() {});
+      } 
+});
+
 
 
 
